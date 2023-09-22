ac4667157ed0e32000697d82bd9223abf2d4a4e7
Yeferson Diaz <yjavierdiazb@gmail.com>
2023-09-22 15:26:03 -0500

    Añadiendo HuespedesDAO

--- src/main/java/co/com/hotelAlura/jdbc/controller/HuespedesController.java
+++ src/main/java/co/com/hotelAlura/jdbc/controller/HuespedesController.java
@@ -5,4 +5,8 @@ import java.sql.SQLException;
 import java.util.Date;
 
 public class HuespedesController {
+
+    private HuespedesDAO huespedesDAO;
+
+    public HuespedesController(HuespedesDAO huespedesDAO){this.huespedesDAO = huespedesDAO;}
 }
--- src/main/java/co/com/hotelAlura/jdbc/dao/HuespedesDAO.java
+++ src/main/java/co/com/hotelAlura/jdbc/dao/HuespedesDAO.java
@@ -5,11 +5,24 @@ import co.com.hotelAlura.jdbc.model.HuespedesModel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.ResultSet;
 
 public class HuespedesDAO {
 
+    public void agregarHuesped(HuespedesModel huespedesModel) throws SQLException{
+        String sql = "INSERT INTO huespedes (nombre, apellido, fecha_nacimiento, nacionalidad, telefono) VALUES (?, ?, ?, ?, ?)";
 
+        try(Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql)){
+            statement.setString(1, huespedesModel.getNombre());
+            statement.setString(2, huespedesModel.getApellido());
+            statement.setDate(3, new java.sql.Date(huespedesModel.getFecha_nacimiento().getTime()));
+            statement.setString(4,huespedesModel.getNacionalidad());
+            statement.setInt(5,huespedesModel.getTelefono());
 
-
+            statement.executeUpdate();
+        }   catch (SQLException e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
 }
