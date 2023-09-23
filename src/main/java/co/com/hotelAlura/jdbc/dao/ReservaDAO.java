61ca754d11c782ba2bb67fb639ec5d502b133038
Yeferson Diaz <yjavierdiazb@gmail.com>
2023-09-23 10:44:37 -0500

    "Se agregan metodos para poder consultar los huespedes en el RESERVADAO"

--- src/main/java/co/com/hotelAlura/jdbc/dao/ReservaDAO.java
+++ src/main/java/co/com/hotelAlura/jdbc/dao/ReservaDAO.java
@@ -1,6 +1,7 @@
 package co.com.hotelAlura.jdbc.dao;
 
 import co.com.hotelAlura.jdbc.factory.ConnectionFactory;
+import co.com.hotelAlura.jdbc.model.HuespedesModel;
 import co.com.hotelAlura.jdbc.model.ReservasModel;
 import java.util.ArrayList;
 
@@ -89,6 +90,54 @@ public class ReservaDAO {
 
 
 
+    public List<HuespedesModel> obtenerTodosHuespedes() throws SQLException{
+        String sql = "SELECT * FROM huespedes";
+        List<HuespedesModel> huespedes = new ArrayList<>();
+        try(Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql)){
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()){
+                    HuespedesModel huesped = new HuespedesModel();
+                    huesped.setId(resultSet.getInt("id"));
+                    huesped.setNombre(resultSet.getString("nombre"));
+                    huesped.setApellido(resultSet.getString("apellido"));
+                    huesped.setFecha_nacimiento(resultSet.getDate("fecha_nacimiento"));
+                    huesped.setNacionalidad(resultSet.getString("nacionalidad"));
+                    huesped.setTelefono(resultSet.getString("telefono"));
+                    huesped.setId_reserva(resultSet.getInt("id_reserva"));
+                    huespedes.add(huesped);
+                }
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+                throw e;
+            }
+        }
+        return huespedes;
+    }
+
+    public List<HuespedesModel> obtenerHuespedesFiltro(int id) throws SQLException{
+        String sql = "SELECT * FROM huespedes WHERE id = ?";
+        List<HuespedesModel> huespedes = new ArrayList<>();
+        try(Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql)){
+            statement.setInt(1,id);
+            try (ResultSet resultSet = statement.executeQuery()){
+                while (resultSet.next()) {
+                    HuespedesModel huesped = new HuespedesModel();
+                    huesped.setId(resultSet.getInt("id"));
+                    huesped.setNombre(resultSet.getString("nombre"));
+                    huesped.setApellido(resultSet.getString("apellido"));
+                    huesped.setFecha_nacimiento(resultSet.getDate("fecha_nacimiento"));
+                    huesped.setNacionalidad(resultSet.getString("nacionalidad"));
+                    huesped.setTelefono(resultSet.getString("telefono"));
+                    huesped.setId_reserva(resultSet.getInt("id_reserva"));
+                    huespedes.add(huesped);
+                }
+            }
+        }
+        return huespedes;
+    }
     public void eliminarReserva(ReservasModel reservasModel) throws SQLException{
         String sql = "DELETE FROM reservas WHERE id = ?";
         try (Connection conn = ConnectionFactory.getConnection();
